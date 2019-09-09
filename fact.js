37019e7e3d240639a0eab62b33d0601556050e66
Sam Naish <sam.naish92@googlemail.com>
2019-09-09 18:20:52 +0100

    move a function for logo

--- fact.js
+++ fact.js
@@ -3,11 +3,16 @@ const randomFact = () => {
     let request = new XMLHttpRequest();
     let api = "https://api.chucknorris.io/jokes/random";
     request.onreadystatechange = () => {
-        if (this.readyState == 4 && this.status == 200) { // HTTP 200 response 
+        if (this.readyState === 4 && this.status === 200) { // HTTP 200 response 
             let json = JSON.parse(this.responseText);
             parseResponse(json);
         }
     };
+
+    console.log('================');
+    console.log('api', api);
+    console.log('================');
+    
 
     request.open("GET", api, true); 
     request.send(); //Call Chuck Norris API for random facts
@@ -17,9 +22,10 @@ const parseResponse = (json) => {
     //JSON response - simple object with a value property contianing the random fact.
     const fact = "<b>" + json["value"] + "</b>";
     getElementById("data").innerHTML = fact;
-    document.getElementById("logo").addEventListener("click", function() {
-        randomFact();
-      });
 }
+
+document.getElementById("logo").addEventListener("click", function() {
+    randomFact();
+  });
 
 randomFact();
